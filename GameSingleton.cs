d81b3ed67929715559e900b0937026f394fa3b11
AlexLemminG <psp_4ever@mail.ru>
2019-01-06 18:51:39 +0300

    Singletones

---
+++ GameSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+    private static T s_instance;
+    private static object s_lock = new object ();
+
+    public static T instance {
+        get {
+            if (s_instance == null) {
+                lock (s_lock) {
+                    if (s_instance == null) {
+                        s_instance = FindObjectOfType<T> ();
+
+                        if (s_instance == null) {
+                            GameObject singleton = new GameObject ();
+                            s_instance = singleton.AddComponent<T> ();
+                            singleton.name = "(singleton) " + typeof (T).ToString ();
+
+                            DontDestroyOnLoad (singleton);
+                            Debug.Log ("[Singleton] '" + singleton + "' created implicitly.");
+                        }
+                    }
+                }
+            }
+            return s_instance;
+        }
+    }
+}
