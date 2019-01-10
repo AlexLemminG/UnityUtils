571e0607d344c5031fb41c9ce0cc217a104ffd9b
AlexLemminG <psp_4ever@mail.ru>
2019-01-10 23:29:06 +0300

    EditorButton attribute

---
+++ EditorButton/Editor/EditorButton.cs
@@ -0,0 +1,36 @@
+﻿// Initial Concept by http://www.reddit.com/user/zaikman
+// Revised by http://www.reddit.com/user/quarkism
+
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof (MonoBehaviour), true)]
+[CanEditMultipleObjects]
+public class EditorButton : Editor {
+	public override void OnInspectorGUI () {
+		base.OnInspectorGUI ();
+
+		var mono = target as MonoBehaviour;
+
+		if (mono == null)
+			return;
+
+		var methods = mono.GetType ()
+			.GetMembers (BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+			.Where (o => Attribute.IsDefined (o, typeof (EditorButtonAttribute)));
+
+		bool guiEnabled = GUI.enabled;
+		foreach (var memberInfo in methods) {
+			var editorButtonAttribute = memberInfo.GetCustomAttribute<EditorButtonAttribute> ();
+			GUI.enabled = editorButtonAttribute.enabledInEditMode || Application.isPlaying;
+			if (GUILayout.Button (memberInfo.Name)) {
+				var method = memberInfo as MethodInfo;
+				method.Invoke (mono, null);
+			}
+		}
+		GUI.enabled = guiEnabled;
+	}
+}
