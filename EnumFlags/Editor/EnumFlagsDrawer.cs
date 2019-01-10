4512f4fccb9588d5b05aa6a2c2dac0ef4a30b24c
AlexLemminG <psp_4ever@mail.ru>
2019-01-11 01:25:56 +0300

    EnumFlagsDrawer

---
+++ EnumFlags/Editor/EnumFlagsDrawer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer (typeof (Enum), true)]
+public class EnumFlagsDrawer : PropertyDrawer {
+	public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
+		EditorGUI.BeginProperty (position, label, property);
+
+		Type type = fieldInfo.FieldType;
+		if (type.GetCustomAttribute<System.FlagsAttribute> () == null) {
+			EditorGUI.PropertyField (position, property, label);
+		} else {
+			Enum enumValue = (Enum) Enum.ToObject (fieldInfo.FieldType, property.intValue);
+			enumValue = EditorGUI.EnumFlagsField (position, label, enumValue);
+			property.intValue = (int) Convert.ToInt32 (enumValue);
+		}
+
+		EditorGUI.EndProperty ();
+	}
+}
