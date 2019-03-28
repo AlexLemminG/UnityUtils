64f1de707695a19c66cac6cb5671d144fe872d83
AlexLemminG <psp_4ever@mail.ru>
2019-03-28 19:50:00 +0600

    GetRangeIndex function in MathUtils

---
+++ MathUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathUtils {
+    //TODO avoid creating array by creating multiple versions
+    public static int GetRangeIndex (float val, params float[] ranges) {
+        for (int i = 0; i < ranges.Length; i++) {
+            if (val < ranges[i]) {
+                return i - 1;
+            }
+        }
+        return ranges.Length - 1;
+    }
+}
